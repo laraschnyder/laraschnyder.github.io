a593d9836dcbc870abf7b78c07076312b8d4b8d4
laraschnyder <31204529+laraschnyder@users.noreply.github.com>
2017-11-04 20:14:01 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  'use strict';
 $(".burger-nav").on("click", function(){
 
-$("header nav ul").toggleClass("open");
+$("header .nav ul").toggleClass("open");
 
 					
 					});
